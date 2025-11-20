7a1c82bf79de08faed7c111f5121fee57032f6a5
tomi108 <tomas.kusnirik@gmail.com>
2025-11-20 10:58:19 +0000

    Add Vite alias @ to src

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/', // pre Vercel
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // <-- this maps "@" to your src folder
+    },
+  },
 })
